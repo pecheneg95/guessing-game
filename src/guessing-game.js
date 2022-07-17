8806e6bed826050d7a7bdcb2044689acd4fd5df7
Artyom <pechera20@gmail.com>
2022-07-17 14:26:18 +0300

    feat: completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.start = 0;
+    this.end = 0;
+    this.gue = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.start = min;
+    this.end = max;
+  }
 
-    }
+  guess() { 
+    return this.gue = Math.ceil((this.start + this.end) / 2)
+  }
 
-    guess() {
+  lower() {
+    this.end = this.gue;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.start = this.gue;
+  }
 }
 
 module.exports = GuessingGame;
